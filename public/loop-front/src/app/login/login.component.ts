d461fee20addbef6b7471759b75c3c51b5a36e6d
Daniela Flores <kenzie85@hotmail.com>
2018-11-16 15:02:39 -0600

    Login update 2

--- public/loop-front/src/app/login/login.component.ts
+++ public/loop-front/src/app/login/login.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() {
-  	document.body.style.background = 'rgb(0,0,0)';
+  	document.body.style.background = 'rgb(15,15,15)';
   }
 
   ngOnInit() {
