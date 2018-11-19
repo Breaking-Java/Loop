38b402d4dd093069ddcbf0af2dcf1d9f9f1846b5
Arturo Burela <uno@arturoburela.com>
2018-11-19 13:16:58 -0600

    Rearrange Files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { ItemDetailComponent } from './components/item-detail/item-detail.component';
 import { CatalogComponent } from './components/catalog/catalog.component';
+import { CatalogPageComponent } from './components/catalog-page/catalog-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     ItemDetailComponent,
-    CatalogComponent
+    CatalogComponent,
+    CatalogPageComponent
   ],
   imports: [
     BrowserModule,
